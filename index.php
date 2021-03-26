e6837c52508fb9c9b0f52879224e3a495c71601c
mzw86020424 <mzw86020424@gmail.com>
2021-03-26 16:48:36 +0900

    htmlとcssを作成

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
+<link rel="stylesheet" href="stylesheet.css">
 <?php
 class Menu {
   public $name;
-
+  
   public function __construct($name) {
     $this->name = $name;
   }
-
+  
   public function hello() {
-    echo "私は{$this->name}です";
+    echo '私は'.$this->name.'です';
   }
 }
 
+$juice = new Menu('JUICE');
+$coffee = new Menu('COFFEE');
 $curry = new Menu('CURRY');
 $pasta = new Menu('PASTA');
+// 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入してください
+$menus = array($juice, $coffee, $curry, $pasta);
 
 ?>
 
-<p><?php echo $curry->name ?></p>
-<p><?php echo $pasta->name ?></p>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <?php foreach($menus as $menu) : ?>
+        <h3><?php echo $menu->name ?></h3>
+      <?php endforeach ?>
+    </div>
+  </div>
+</body>
+</html>
