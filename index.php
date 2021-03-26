1be79fa7c201c66a7c1153c3f17c5a063eb5b470
mzw86020424 <mzw86020424@gmail.com>
2021-03-26 16:30:54 +0900

    Menuクラスのnameプロパティを作成

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
 class Menu {
+  public $name;
 }
 
 $curry = new Menu();
 $pasta = new Menu();
 
+$curry->name = 'CURRY';
+echo $curry->name;
+
+$pasta->name = 'PASTA';
+echo $pasta->name;
 ?>
