4b39834b1b920aa1f9a55acfd3cb08060bb103f4
mzw86020424 <mzw86020424@gmail.com>
2021-03-26 16:33:48 +0900

    Menuクラスのhelloメソッドを作成

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-
 class Menu {
   public $name;
+
+  public function hello() {
+    echo "私はMenuクラスのインスタンスです";
+  }
 }
 
 $curry = new Menu();
@@ -9,7 +12,9 @@ $pasta = new Menu();
 
 $curry->name = 'CURRY';
 echo $curry->name;
+$curry->hello();
 
 $pasta->name = 'PASTA';
 echo $pasta->name;
+$pasta->hello();
 ?>
