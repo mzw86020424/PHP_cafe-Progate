97b413f4b51368ad5ba8fc3443c51df3af9a2966
mzw86020424 <mzw86020424@gmail.com>
2021-03-28 08:09:26 +0900

    メニューに紐づいたレビューのみを表示

--- data.php
+++ data.php
@@ -10,9 +10,15 @@ $pasta = new Food('PASTA',1200,'https://s3-ap-northeast-1.amazonaws.com/progate/
 
 $menus = array($juice, $coffee, $curry, $pasta);
 
-$review1 = new Review($juice->getName(),'果肉たっぷりのオレンジジュースです！');
-$review2 = new Review($curry->getName(),'具がゴロゴロしていてとてもおいしいです');
+$review1 = new Review($juice->getName(), '果肉たっぷりのオレンジジュースです！');
+$review2 = new Review($curry->getName(), '具がゴロゴロしていてとてもおいしいです');
+$review3 = new Review($coffee->getName(), '香りがいいです');
+$review4 = new Review($pasta->getName(), 'ソースが絶品です。また食べたい。');
+$review5 = new Review($juice->getName(), '普通のジュース');
+$review6 = new Review($curry->getName(), '値段の割においしいカレーだと思いました');
+$review7 = new Review($coffee->getName(), '苦味がちょうどよくて、おすすめです');
+$review8 = new Review($pasta->getName(), '具材にこだわりを感じました。');
 
-$reviews = array($review1,$review2);
+$reviews = array($review1, $review2, $review3, $review4, $review5, $review6, $review7, $review8);
 
 ?>
--- show.php
+++ show.php
@@ -5,6 +5,7 @@ require_once('data.php');
 $menuName = $_GET['name'];
 
 $menu = Menu::findByName($menus, $menuName);
+$menuReviews = $menu->getReviews($reviews);
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +39,7 @@ $menu = Menu::findByName($menus, $menuName);
             <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/review.png" class='icon-review'>
             <h4>レビュー一覧</h4>
           </div>
-          <?php foreach ($reviews as $review) : ?>
+          <?php foreach ($menuReviews as $review) : ?>
             <h3><?php echo $review->getMenuName() ?></h3>
             <p><?php echo $review->getBody() ?></p>
           <?php endforeach ?>
