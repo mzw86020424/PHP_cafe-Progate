872115e52d67380923d77c775f1b070ec469c127
mzw86020424 <mzw86020424@gmail.com>
2021-03-27 09:39:42 +0900

    parentを使用

--- drink.php
+++ drink.php
@@ -5,12 +5,8 @@ class Drink extends Menu {
   private $type;
 
   public function __construct($name, $price, $image, $type) {
-    $this->name = $name;
-    $this->price = $price;
-    $this->image = $image;
+    parent::__construct($name, $price, $image);
     $this->type = $type;
-    self::$count++;
-    
   }
 
   public function getType() {
